935ad729c6017427bb4cfdba8b2f6d3e48c09d25
fjbanezares <fjbanezares@gmail.com>
2024-11-25 14:06:44 +0100

    javi

--- 6. Express/3. Postman/index.js
+++ 6. Express/3. Postman/index.js
@@ -18,15 +18,15 @@ app.post("/register", (req, res) => {
   res.sendStatus(201);
 });
 
-app.put("/user/angela", (req, res) => {
+app.put("/user/javi", (req, res) => {
   res.sendStatus(200);
 });
 
-app.patch("/user/angela", (req, res) => {
+app.patch("/user/javi", (req, res) => {
   res.sendStatus(200);
 });
 
-app.delete("/user/angela", (req, res) => {
+app.delete("/user/javi", (req, res) => {
   //Deleting
   res.sendStatus(200);
 });
